ba4758a03267333c2073f0054301e7e462a62ff0
Darshan Hegde <darshanhegde1403@gmail.com>
2026-01-20 00:12:39 +0530

    Update index.js

--- pro6/frontend/src/index.js
+++ pro6/frontend/src/index.js
@@ -8,9 +8,8 @@ import Inventory from './inventory';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-{/* <Display_Inventory /> 
-<AddInventory /> */}
-<Inventory />
+<Display_Inventory /> 
+<AddInventory /> 
  </React.StrictMode>
 
 );
